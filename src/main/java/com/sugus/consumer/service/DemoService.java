17df98902a1a4fa57955f85d89f4e9a1981bff32
suguswong <suguswong@outlook.com>
2019-08-05 09:38:55 +0800

    add consumer

---
+++ src/main/java/com/sugus/consumer/service/DemoService.java
@@ -0,0 +1,15 @@
+package com.sugus.consumer.service;
+
+import com.sugus.consumer.service.hystrix.DemoServiceHystrix;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+@Component
+@FeignClient(name = "producer", fallback = DemoServiceHystrix.class)
+public interface DemoService {
+
+    @GetMapping("/demoProducer/getHello")
+    String getHello(@RequestParam(value = "name", required = false) String name);
+
+}
